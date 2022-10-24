afab088a11ee28b84a5d8aa193b51ffb1a196d34
hwolfinger <haroldwolfingerIT@gmail.com>
2022-10-24 09:53:45 -0400

    Ln88 assert_raise(NoMethodError) do >>> solution

--- about_symbols.rb
+++ about_symbols.rb
@@ -85,8 +85,8 @@ class AboutSymbols < Neo::Koan
 
   def test_symbols_cannot_be_concatenated
     # Exceptions will be pondered further down the path
-    assert_raise(CustomError) do
-      raise CustomError, ':cats + :dogs'
+    assert_raise(NoMethodError) do
+      :cats + :dogs
     end
   end
 
